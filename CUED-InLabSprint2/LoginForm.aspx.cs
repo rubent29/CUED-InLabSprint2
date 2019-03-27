e6227ef89b011e9222f49d0cff981b69e813dd5d
Ruben Torrico <torricre@dukes.jmu.edu>
2019-03-27 17:53:29 -0400

    Login Form

--- CUED-InLabSprint2/LoginForm.aspx.cs
+++ CUED-InLabSprint2/LoginForm.aspx.cs
@@ -34,14 +34,54 @@ public partial class Login_v3_LoginForm : System.Web.UI.Page
 
     protected void Login_Click(object sender, EventArgs e)
     {
+        {
+            //// connect to database to retrieve stored password string
+            //try
+            //{
+                System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection();
+                sc.ConnectionString = @"Server=LOCALHOST;Database=CuedIn;Trusted_Connection=Yes;";
+                lblStatus.Text = "Database Connection Successful";
 
- 
-                        Response.Redirect("CUED-InHomeForm.aspx");
+                sc.Open();
+                System.Data.SqlClient.SqlCommand findPass = new System.Data.SqlClient.SqlCommand();
+                findPass.Connection = sc;
+                // SELECT PASSWORD STRING WHERE THE ENTERED USERNAME MATCHES
+                findPass.CommandText = "select Password from Account where Username = @Username";
+                findPass.Parameters.Add(new SqlParameter("@Username", Username.Text));
 
+                SqlDataReader reader = findPass.ExecuteReader(); // create a reader
+
+                if (reader.HasRows) // if the username exists, it will continue
+                {
+                    while (reader.Read()) // this will read the single record that matches the entered username
+                    {
+                        string storedHash = reader["Password"].ToString(); // store the database password into this variable
+
+                        if (PasswordHash.ValidatePassword(Password.Text, storedHash)) // if the entered password matches what is stored, it will show success
+                        {
+                            lblStatus.Text = "Success!";
+                            Response.Redirect("CUED-InHomeForm.aspx");
+                        }
+                        else
+                            lblStatus.Text = "Password is wrong.";
+                    }
+                }
+                else // if the username doesn't exist, it will show failure
+                    lblStatus.Text = "Login failed.";
+
+                sc.Close();
+            //}
+            //catch
+            //{
+            //    lblStatus.Text = "Database Error.";
+            //}
+        }
     }
+
 
     protected void ForgetPasswordLink_Click(object sender, EventArgs e)
     {
 
     }
+
 }
