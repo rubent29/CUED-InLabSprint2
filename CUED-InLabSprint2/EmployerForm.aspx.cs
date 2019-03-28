05aa7f7cb9a3a015537fbca983a32e34cc23ea70
James Elliot <elliotjd@dukes.jmu.edu>
2019-03-27 20:47:04 -0400

    Made Employer Form The Create User Form

--- CUED-InLabSprint2/EmployerForm.aspx.cs
+++ CUED-InLabSprint2/EmployerForm.aspx.cs
@@ -34,9 +34,11 @@ public partial class EmployerForm1 : System.Web.UI.Page
 
     protected void Insert_Button_Click(object sender, EventArgs e)
     {
+        if (FirstName.Text != "" && LastName.Text != "" && CompanyName.Text != "" && CompanyEmail.Text != "" && StreetAddress.Text != "" && City.Text != "" && Country.Text != ""
+            && ZipCode.Text != "" && PasswordOne.Text != "" && PasswordTwo.Text != "")
+        {
 
-
-        Employer emp = new Employer(HttpUtility.HtmlEncode(FirstName.Text.Trim()), HttpUtility.HtmlEncode(LastName.Text.Trim()), HttpUtility.HtmlEncode(CompanyName.Text.Trim()),
+            Employer emp = new Employer(HttpUtility.HtmlEncode(FirstName.Text.Trim()), HttpUtility.HtmlEncode(LastName.Text.Trim()), HttpUtility.HtmlEncode(CompanyName.Text.Trim()),
                                     HttpUtility.HtmlEncode(CompanyEmail.Text.Trim()), HttpUtility.HtmlEncode(StreetAddress.Text.Trim()), HttpUtility.HtmlEncode(City.Text.Trim()), HttpUtility.HtmlEncode(State.Text.Trim()),
                                     HttpUtility.HtmlEncode(Country.SelectedItem.Text.Trim()), HttpUtility.HtmlEncode(ZipCode.Text.Trim()), HttpUtility.HtmlEncode(PasswordOne.Text.Trim()), HttpUtility.HtmlEncode(PasswordTwo.Text.Trim()),
                                     HttpUtility.HtmlEncode(LastUpdatedBy), HttpUtility.HtmlEncode(LastUpdated));
@@ -44,30 +46,54 @@ public partial class EmployerForm1 : System.Web.UI.Page
 
 
 
-        System.Data.SqlClient.SqlCommand MaxStudent = new System.Data.SqlClient.SqlCommand();
-        MaxStudent.Connection = DBconnection;
+            System.Data.SqlClient.SqlCommand MaxStudent = new System.Data.SqlClient.SqlCommand();
+            MaxStudent.Connection = DBconnection;
 
-        DBconnection.Open();
+            DBconnection.Open();
 
-        string employerInfo = "insert into [dbo].[Employer] values (@firstName, @lastName, @companyName, @companyEmail, @streetAddress, @city, @state, @country, @zipCode, @passwordOne, @passwordTwo, @lastUpdatedBy, @lastUpdated)";
-        SqlCommand insertEmployer = new SqlCommand(employerInfo, DBconnection);
-        insertEmployer.Parameters.AddWithValue("@firstName", emp.getFirstName());
-        insertEmployer.Parameters.AddWithValue("@lastName", emp.getLastName());
-        insertEmployer.Parameters.AddWithValue("@companyName", emp.getCompanyName());
-        insertEmployer.Parameters.AddWithValue("@companyEmail", emp.getCompanyEmail());
-        insertEmployer.Parameters.AddWithValue("@streetAddress", emp.getStreetAddress());
-        insertEmployer.Parameters.AddWithValue("@city", emp.getCity());
-        insertEmployer.Parameters.AddWithValue("@state", emp.getState());
-        insertEmployer.Parameters.AddWithValue("@country", emp.getCountry());
-        insertEmployer.Parameters.AddWithValue("@zipCode", emp.getZipCode());
-        insertEmployer.Parameters.AddWithValue("@passwordOne", emp.getPasswordOne());
-        insertEmployer.Parameters.AddWithValue("@passwordTwo", emp.getPasswordTwo());
-        insertEmployer.Parameters.AddWithValue("@lastUpdatedBy", emp.getLastUpdatedBy());
-        insertEmployer.Parameters.AddWithValue("@lastUpdated", emp.getLastUpdated());
+            string employerInfo = "insert into [dbo].[Employer] values (@firstName, @lastName, @companyName, @companyEmail, @streetAddress, @city, @state, @country, @zipCode, @passwordOne, @passwordTwo, @lastUpdatedBy, @lastUpdated)";
+            SqlCommand insertEmployer = new SqlCommand(employerInfo, DBconnection);
+            insertEmployer.Parameters.AddWithValue("@firstName", emp.getFirstName());
+            insertEmployer.Parameters.AddWithValue("@lastName", emp.getLastName());
+            insertEmployer.Parameters.AddWithValue("@companyName", emp.getCompanyName());
+            insertEmployer.Parameters.AddWithValue("@companyEmail", emp.getCompanyEmail());
+            insertEmployer.Parameters.AddWithValue("@streetAddress", emp.getStreetAddress());
+            insertEmployer.Parameters.AddWithValue("@city", emp.getCity());
+            insertEmployer.Parameters.AddWithValue("@state", emp.getState());
+            insertEmployer.Parameters.AddWithValue("@country", emp.getCountry());
+            insertEmployer.Parameters.AddWithValue("@zipCode", emp.getZipCode());
+            insertEmployer.Parameters.AddWithValue("@passwordOne", emp.getPasswordOne());
+            insertEmployer.Parameters.AddWithValue("@passwordTwo", emp.getPasswordTwo());
+            insertEmployer.Parameters.AddWithValue("@lastUpdatedBy", emp.getLastUpdatedBy());
+            insertEmployer.Parameters.AddWithValue("@lastUpdated", emp.getLastUpdated());
 
-        insertEmployer.ExecuteNonQuery();
-        DBconnection.Close();
-        DBconnection.Open();
+            insertEmployer.ExecuteNonQuery();
+            DBconnection.Close();
+            DBconnection.Open();
+
+            if (PasswordOne.Text == PasswordTwo.Text)
+            {
+                System.Data.SqlClient.SqlCommand setPass = new System.Data.SqlClient.SqlCommand();
+                setPass.Connection = DBconnection;
+
+                // INSERT PASSWORD RECORD AND CONNECT TO USER
+                setPass.CommandText = "insert into[dbo].[Account] values (@Username, @Password)";
+                setPass.Parameters.Add(new SqlParameter("@Username", CompanyEmail.Text));
+                setPass.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(PasswordOne.Text))); // hash entered password
+                setPass.ExecuteNonQuery();
+
+                DBconnection.Close();
+
+                labelStatus.Text = "User committed!";
+                Response.Redirect("LoginForm.aspx", false);
+            }
+            else
+                labelStatus.Text = "Passwords Do Not Match";
+
+
+        }
+        else
+            labelStatus.Text = "Fill all fields.";
     }
 
     protected void Populate_Button_Click(object sender, EventArgs e)
