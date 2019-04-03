2bf1aa10ce173f383123f0d208f5bdfa9c85aad0
James Elliot <elliotjd@dukes.jmu.edu>
2019-04-02 20:59:22 -0400

    changes to web config mail settings

--- CUED-InLabSprint2/LoginForm.aspx.cs
+++ CUED-InLabSprint2/LoginForm.aspx.cs
@@ -172,5 +172,8 @@ public partial class Login_v3_LoginForm : System.Web.UI.Page
     protected void ForgetPasswordLink_Click(object sender, EventArgs e)
     {
 
+
+        Response.Redirect("ForgotPassword.aspx");
+
     }
 }
