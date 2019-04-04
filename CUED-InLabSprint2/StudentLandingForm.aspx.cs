2d551486f2f6ac8a2c46edcb95e2492a65f51785
James Elliot <elliotjd@dukes.jmu.edu>
2019-04-03 22:24:04 -0400

    Added a student Job Viewing Form, & Added the login redirect to forms

--- CUED-InLabSprint2/StudentLandingForm.aspx.cs
+++ CUED-InLabSprint2/StudentLandingForm.aspx.cs
@@ -9,6 +9,11 @@ public partial class StudentLandingForm : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if ((HttpContext.Current.Request.UrlReferrer == null))
+        {
+            Response.Redirect("LoginForm.aspx");
+        }
+
 
     }
 
