d077b0b735543d2476270b5ec0b51264218dbcad
Sophia Bengzon <bengzoss@dukes.jmu.edu>
2019-04-14 20:14:18 -0400

    fill labels with info (hardcoded using cole's db info)

--- CUED-InLabSprint2/AccountInformationForm.aspx.cs
+++ CUED-InLabSprint2/AccountInformationForm.aspx.cs
@@ -6,19 +6,52 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.ApplicationServices;
+using System.Configuration;
+
 
 public partial class AccountInformationForm : System.Web.UI.Page
 {
+    //open sql connection: use webconfig
+    SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+
+    //Session["Test"].ToString(); //going to set labels using this session variable for the user email
     protected void Page_Load(object sender, EventArgs e)
     {
-        //get employer id info to call from sql
-        //maybe use gridview to display sql data? if not, set labels
-        //use session variable to get sql select?
+
+        connection.Open();
+
+        string cmd = "select * from employer where companyemail = '" + "cole@dukes.com" + "';"; //use session variable irl: rn just testing w cole
+
+
+        SqlDataAdapter adp = new SqlDataAdapter();
+        adp.SelectCommand = new SqlCommand(cmd, connection);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            //set all the labels
+
+            FirstNameLabel.Text = ds.Tables[0].Rows[0].Field<string>(1); //sets first name label
+            Label17.Text = ds.Tables[0].Rows[0].Field<string>(2); //last name
+            CompNameLabel.Text = ds.Tables[0].Rows[0].Field<string>(3); //company
+            CompEmailLabel.Text = ds.Tables[0].Rows[0].Field<string>(4); //email used
+            Label19.Text = ds.Tables[0].Rows[0].Field<string>(5); //address
+            Label20.Text = ds.Tables[0].Rows[0].Field<string>(6); //city
+            Label21.Text = ds.Tables[0].Rows[0].Field<string>(7); //state
+            Label22.Text = ds.Tables[0].Rows[0].Field<string>(8); //country
+            Label23.Text = ds.Tables[0].Rows[0].Field<string>(9); //zip
+            //not showing any password or security question stuff
+            
+        }
+        connection.Close();
+
+
     }
-
-
-    protected void LoginAgainButton_Click(object sender, EventArgs e)
+            protected void LoginAgainButton_Click(object sender, EventArgs e)
     {
+        //should this go right to the editaccountinfo page? this seems uneccessary 
         Response.Redirect("EditAccountLoginForm.aspx");
     }
 }
